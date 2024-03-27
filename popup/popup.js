6b4cf7a4d10947426778cdff32233094fd3b13ba
mvilla9 <mateo.john.villa@gmail.com>
2024-03-26 23:28:28 -0400

    Add brightness slider and display value

--- popup/popup.js
+++ popup/popup.js
@@ -1,1 +1,7 @@
-console.log("Logging to console...")
+let slider = document.getElementById("brightnessSlider");
+let brightnessLabel = document.getElementById("brightnessLabel");
+brightnessLabel.innerHTML = slider.value;
+
+slider.oninput = function displaySliderValue() {
+    brightnessLabel.innerHTML = slider.value;
+}
